84697ffbb08ec288979b988aee35b8510010b4de
yuki okamoto <yukiokamoto@MacBook-Pro-2.local>
2021-07-13 01:41:49 +0900

    added dynamic tweet component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Tweet from "./Tweet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const [users, setUsers] = useState([
+    {name: "yuki", message: "Hello there!"},
+    {name: "John Snow", message: "I am the true King!"},
+    {name: "Edward Stark", message: "Winter is Coming!"},
+  ])
+
+  return(
+    <div className="app">
+      {users.map(user =>(
+        <Tweet name={user.name} message={user.message} />
+      ))}
     </div>
   );
 }
+
 
 export default App;
